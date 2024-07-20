df6cf806fcdd73d536dc1aa9171dca0eed6da129
Meliani-Mehdi <mehdi.hatachi@gmail.com>
2024-07-20 17:11:57 +0100

    added stuff

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, jsonify, request
+from flask import redirect, render_template, request
 from main import app
 import sqlite3
 
@@ -13,4 +13,21 @@ def types():
 
 @app.route("/types/add", methods=["POST", "GET"])
 def addtypes():
+    if request.method == "POST":
+        conn = sqlite3.connect('finance.db')
+        cursor = conn.cursor()
+        name = request.form.get("name")
+        cursor.execute("INSERT INTO type(name) values(?)", (name, ))
+        conn.commit()
+        conn.close()
+        return redirect('/types')
     return render_template('add_types.html')
+
+@app.route("/types/list")
+def listtypes():
+    conn = sqlite3.connect('finance.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM type")
+    types = cursor.fetchall()
+
+    return render_template('list_types.html', types = types)
